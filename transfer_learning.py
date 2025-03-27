d05ab2230b383bcab6d9765a974f212481c7ef5e
Daniel Strick <danstrick17@gmail.com>
2025-03-27 14:31:56 -0400

    ancienct-voice-54 from transfer_learning.py

--- transfer_learning.py
+++ transfer_learning.py
@@ -115,7 +115,6 @@ def main():
     #      Data Transformation (MOdified for ResNet) Only augment training data, not test data
     transform_train = transforms.Compose([
         transforms.ToTensor(),
-
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
     ])
 
